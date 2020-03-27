a0f27d87039136387f600a281d118e684fbe6d3e
Travis Scholl <traviswscholl@gmail.com>
2020-03-27 07:52:02 -0400

    formattig

--- split.sql
+++ split.sql
@@ -1,24 +1,24 @@
 /*
  * Splits a string on whitespace characters.
  */
-WITH RECURSIVE split(word,prev,rest) AS (
-    VALUES('','','hello world')
-    UNION ALL
-    SELECT
-        -- 20=space,0a=newline,0d=carriagereturn,09=tab
-        CASE WHEN instr(x'200A0D09',prev)
-            THEN ''
-            ELSE word||prev
-        END,
-        substr(rest,0,2), -- last character seen
-        substr(rest,2) -- skips one character
-    FROM split
-    WHERE prev != '' OR rest != ''
-)
-SELECT
-    word
-FROM
-    split
-WHERE
-    word!='' AND (instr(x'200A0D09',prev) OR prev='')
+WITH RECURSIVE
+    split(idx,word,prev,rest) AS (
+        VALUES(0,'','','hello world')
+        UNION ALL
+        SELECT
+            idx+1,
+            -- 20=space,0a=newline,0d=carriagereturn,09=tab
+            CASE WHEN instr(x'200A0D09',prev)
+                THEN ''
+                ELSE word||prev
+            END,
+            substr(rest,0,2), -- last character seen
+            substr(rest,2) -- skips one character
+        FROM split WHERE prev != '' OR rest != ''
+    ),
+    words(w,idx) AS (
+        SELECT word,idx-length(word) FROM split
+        WHERE word!='' AND (instr(x'200A0D09',prev) OR prev='')
+    )
+SELECT * FROM words
 ;
